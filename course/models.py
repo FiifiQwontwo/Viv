216bf7f209107970aa1ebb7e2c2f752e6cbeca6d
FiifiQwontwo <quophi555@gmail.com>
2024-03-04 17:21:48 +0000

    Initial commit

---
+++ course/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from djangoProject12.slugify import slugify
+from faculty.models import Faculty
+
+
+# Create your models here.
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=100)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Course'
+
+    def __str__(self):
+        return self.course_name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.course_name)
+        super(Course, self).save(*args, **kwargs)
