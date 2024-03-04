2f02651198210f2c1977204d3360590d44ca4d38
FiifiQwontwo <quophi555@gmail.com>
2024-03-04 22:53:52 +0000

    add student and Lecturer forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def get_user_agents(request):
+    return request.META.get('HTTP_USER_AGENT', '')
+
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
