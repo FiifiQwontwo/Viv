e3ccbff9189c0f0277aee4dbe1bba78190024f9d
FiifiQwontwo <quophi555@gmail.com>
2024-03-04 17:44:07 +0000

    admin

--- faculty/admin.py
+++ faculty/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Faculty
 from course.models import Course
+from accounts.models import Student, Lecturer, CustomUser
 
 # Register your models here.
 
@@ -22,3 +23,34 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ['course_name', 'faculty']
     list_per_page = 20
     prepopulated_fields = {'slug': ('course_name',)}
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'index_number', 'level', 'course')
+    list_filter = ('level', 'course')
+    search_fields = ('first_name', 'last_name', 'index_number')
+    ordering = ('last_name', 'first_name')
+    fieldsets = ()
+
+
+@admin.register(Lecturer)
+class LecturerAdmin(admin.ModelAdmin):
+    list_display = ('title', 'last_name', 'first_name')
+    list_filter = ('faculty',)
+    search_fields = ('last_name', 'faculty__faculty_name')
+    ordering = ('title',)
+    fieldsets = ()
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email',  'last_login', 'date_joined')
+    list_display_links = ('email',)
+    readonly_fields = ('last_login', 'date_joined')
+    ordering = ('-date_joined',)
+    # Used so the other required fields such as groups is disabled
+    filter_horizontal = ()
+    list_filter = ()
+    # Used to make password read only
+    fieldsets = ()
