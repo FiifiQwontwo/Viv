e4b728bdd0dc9f035d8d63f8414acc6d7c1716da
FiifiQwontwo <quophi555@gmail.com>
2024-03-26 23:54:45 +0000

    Signup works now for Lecturer

--- document/models.py
+++ document/models.py
@@ -1,9 +1,24 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 from chapter.models import Chapter
+from accounts.models import Student
 
 
 # Create your models here.
 
 class Document(models.Model):
     title = models.CharField(max_length=100)
-    content = models.FileField()
+    content = models.FileField(upload_to='documents/%Y/%m/%d/', validators=[
+        FileExtensionValidator(allowed_extensions=['pdf', 'doc', 'docx'])
+    ])
+    chapter = models.ForeignKey(Chapter, on_delete=models.RESTRICT)
+    student = models.ForeignKey(Student, on_delete=models.RESTRICT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Document'
+        verbose_name_plural = 'Document'
+
+    def __str__(self):
+        return f"Document for {self.student.last_name + ' ' + self.student.first_name}"
