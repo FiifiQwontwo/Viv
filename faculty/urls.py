b96434538276ac71d7544755cacbd90ef091d0d6
FiifiQwontwo <quophi555@gmail.com>
2024-04-08 21:09:16 +0000

    Reviews

--- faculty/urls.py
+++ faculty/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import listFaculty
+from .views import listFaculty, detailsFaculty
 
 app_name = 'faculty'
 
 urlpatterns =[
-      path('faculty/', listFaculty, name='facultyList_urls')
+      path('faculty/', listFaculty, name='facultyList_urls'),
+      path('faculty/<slug:slug>/', detailsFaculty, name='faculty_details_urls')
 ]
--- faculty/views.py
+++ faculty/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Faculty
 from accounts.decorators import staff_required
 
@@ -12,3 +13,10 @@ def listFaculty(request):
 
     }
     return render(request, 'admin/faculty.html', context)
+
+
+def detailsFaculty(request, slug):
+    facs = get_object_or_404(Faculty, slug=slug)
+    if facs is None:
+        raise Http404()
+    return render(request, 'admin/facultydetails.html', {'fac': facs})
