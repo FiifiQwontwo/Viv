72954b2321b3d4abd81279c86780803f94cc8720
FiifiQwontwo <quophi555@gmail.com>
2024-04-17 01:21:31 +0000

    work on the documents upload and also review page

--- document/forms.py
+++ document/forms.py
@@ -1,8 +1,13 @@
 from django import forms
+from accounts.models import Student
+from chapter.models import Chapter
 from .models import Document
 
 
 class DocumentUploader(forms.ModelForm):
     class Meta:
         model = Document
-        fields = ['title', 'content', 'chapter', 'student']
+        fields = ('title', 'content', 'chapter', 'student')
+
+        chapter = forms.ModelChoiceField(label='chapter', queryset=Chapter.objects.all)
+        student = forms.ModelChoiceField(label='student', queryset=Student.objects.all)
